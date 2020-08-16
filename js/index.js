969f30514c6a8be8a8d4a25a77a06cac7bb322a6
xrf0725 <1099972761@qq.com>
2020-08-16 17:45:23 +0800

    完成主体第二部分

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var subgatter = document.getElementById("subgatter");
+console.log(subgatter);
+var subNavEles = subgatter.getElementsByTagName("a");
+console.log(subNavEles);
+console.log(subNavEles);
+// for(var i=0;i<subNavEles.length;i++){
+//     (function(n){
+//       subNavEles[n].onmouseenter = function(){
+//           for(var j=0;j<subNavEles.length;j++){
+//                 subNavEles[j].className = '';
+//           }
+//           this.className = 'active'
+//       }  
+//     }(i))
+// }
